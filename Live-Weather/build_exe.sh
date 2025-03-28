34ff764cce0f54761e2fbc8a75ab400ae5138927
kee_hoon <djfkfk12345@naver.com>
2025-03-28 10:09:15 +0900

    update documentation

--- Live-Weather/build_exe.sh
+++ Live-Weather/build_exe.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
+TARGET_DIR="Live-Weather"
+EXE_FILE_NAME="Live_Weather"
+
 if [[ "${CURRENT_DIR##*/}" == "Project-Hub" ]]; then
     cd weather
-elif [[ "${CURRENT_DIR##*/}" == "weather" ]]; then
+elif [[ "${CURRENT_DIR##*/}" == ${TARGET_DIR} ]]; then
     true
 else
-    echo "[Error] This script can only be executed in the 'Project-Hub' or 'weather' (in 'Project-Hub') directory."
+    echo "[Error] This script can only be executed in the 'Project-Hub' or ${TARGET_DIR} (in 'Project-Hub') directory."
     exit 1
 fi
 
-echo "The current directory is 'weather'. Starting execution."
-pyinstaller -F -n Live_weather --onefile --icon=마스크.ico app.py
-echo "Build completed: dist/Live_weather.exe"
+echo "The current directory is ${TARGET_DIR}. Starting execution."
+pyinstaller -F -n ${EXE_FILE_NAME} --icon=마스크.ico app.py
+echo "Build completed: dist/${EXE_FILE_NAME}.exe"
  
