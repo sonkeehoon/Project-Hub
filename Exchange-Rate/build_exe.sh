6d71e74e0184c59b109f88c2a6044513f66277b1
kee_hoon <djfkfk12345@naver.com>
2025-03-28 11:19:04 +0900

    Update

---
+++ Exchange-Rate/build_exe.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REPO_NAME="Project-Hub"
+CURRENT_DIR=$(pwd)
+TARGET_DIR="Exchange-Rate"
+EXE_FILE_NAME="Exchange_Rate"
+ICO="exchange_rate1.ico"
+CODE="app.py"
+
+if [[ "${CURRENT_DIR##*/}" == ${REPO_NAME} ]]; then
+    cd ${TARGET_DIR}
+elif [[ "${CURRENT_DIR##*/}" == ${TARGET_DIR} ]]; then
+    true
+else
+    echo "[Error] This script can only be executed in the '${REPO_NAME}' or ${TARGET_DIR} (in '${REPO_NAME}') directory."
+    exit 1
+fi
+
+echo "The current directory is ${TARGET_DIR}. Starting execution."
+pyinstaller -F -n ${EXE_FILE_NAME} --icon=${ICO} ${CODE}
+echo "Build completed: dist/${EXE_FILE_NAME}.exe"
--- Live-Weather/build_exe.sh
+++ Live-Weather/build_exe.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+REPO_NAME="Project-Hub"
 CURRENT_DIR=$(pwd)
 TARGET_DIR="Live-Weather"
 EXE_FILE_NAME="Live_Weather"
+ICO="마스크.ico"
+CODE="app.py"
 
-if [[ "${CURRENT_DIR##*/}" == "Project-Hub" ]]; then
-    cd weather
+if [[ "${CURRENT_DIR##*/}" == ${REPO_NAME} ]]; then
+    cd ${TARGET_DIR}
 elif [[ "${CURRENT_DIR##*/}" == ${TARGET_DIR} ]]; then
     true
 else
-    echo "[Error] This script can only be executed in the 'Project-Hub' or ${TARGET_DIR} (in 'Project-Hub') directory."
+    echo "[Error] This script can only be executed in the '${REPO_NAME}' or ${TARGET_DIR} (in '${REPO_NAME}') directory."
     exit 1
 fi
 
 echo "The current directory is ${TARGET_DIR}. Starting execution."
-pyinstaller -F -n ${EXE_FILE_NAME} --icon=마스크.ico app.py
+pyinstaller -F -n ${EXE_FILE_NAME} --icon=${ICO} ${CODE}
 echo "Build completed: dist/${EXE_FILE_NAME}.exe"
